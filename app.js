c7d0b0495dd3c26ac59e52ec561d9f16b715254d
ValEscoSierra <sarissierra2003@hotmail.com>
2024-01-06 18:56:25 -0500

    Números aceptados en los flujos de respuesta de pedidos

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MockAdapter = require('@bot-whatsapp/database/mock')
 const flujoPedido = addKeyword(['Hola','Buenos días', 'Buenas', '¿Cómo estás?', 'Saludos', '¡Hola, bot!',
     'Hola, ¿estás ahí?', 'Iniciar conversación', 'Empezar chat', '¿Qué tal?', 'Hey', '¿Hola, qué haces?', 'Buen día',
     'Buenas tardes', 'Buenas noches', 'Hello', 'Hi', '¿Hay alguien?', '¿Puedo preguntar algo?',
-    'Hola, ¿me puedes ayudar?', 'buenas', 'hola','1']).addAnswer('🙌 Hola bienvenido a la Freseria🍓').addAnswer(['Sigue este enlace para ver nuestro catálogo en WhatsApp: https://wa.me/c/573134190482','A continuación te preguntaremos los detalles de tu pedido 💌'])
+    'Hola, ¿me puedes ayudar?', 'buenas', 'hola']).addAnswer('🙌 Hola bienvenido a la Freseria🍓').addAnswer(['Sigue este enlace para ver nuestro catálogo en WhatsApp: https://wa.me/c/573134190482','A continuación te preguntaremos los detalles de tu pedido 💌'])
     .addAnswer(['¿Qué deseas ordenar?', 'Recuerda escribir todos los productos que deseas ordenar en un solo mensaje'], { capture: true }, async (ctx, { state, flowDynamic }) => {
         const pedidoCapturado = ctx.body;
         console.log('Pedido capturado:', pedidoCapturado);
@@ -18,21 +18,21 @@ const flujoPedido = addKeyword(['Hola','Buenos días', 'Buenas', '¿Cómo estás
     }).addAnswer(null, null, async (_, { flowDynamic, state }) => {
         const myState = state.getMyState()
         await flowDynamic(`Confirmación del pedido: \n${myState.pedido}`)
-    }).addAnswer(['¿Confirmas tu pedido? Escribe el número de la opción que deseas escoger #️⃣', '1. Si ✅','2. No ❌'],{capture:true},async(ctx, {state, flowDynamic, gotoFlow}) => {
+    }).addAnswer(['¿Confirmas tu pedido? Escribe si o no  de acuerdo a la opción que deseas escoger #️⃣', 'Si ✅','No ❌'],{capture:true},async(ctx, {state, flowDynamic, gotoFlow}) => {
 
         console.log('...')
 
-        if(ctx.body === '2' || ctx.body ==='No' || ctx.body ==='no'){
+        if(ctx.body ==='No' || ctx.body ==='no'){
             await state.clear(['name', 'dir']);
             return gotoFlow(flujoPedido)
-        }else if (ctx.body ==='Si'|| ctx.body ==='si' || ctx.body ==='1'){
+        }else if (ctx.body ==='Si'|| ctx.body ==='si'){
             await flowDynamic(`Productos registrados`)
             return gotoFlow(flujoDatosPedido)
         }
 
     })
 
-const flujoDatosPedido = addKeyword(['1', 'Si'])
+const flujoDatosPedido = addKeyword(['Si', 'si', 'SI'])
 
     .addAnswer('¿Cual es tu nombre?', { capture: true }, async (ctx, { state, flowDynamic }) => {
         const nombreCapturado = ctx.body;
@@ -46,42 +46,31 @@ const flujoDatosPedido = addKeyword(['1', 'Si'])
     }).addAnswer('Tus datos son:', null, async (_, { flowDynamic, state }) => {
         const myState = state.getMyState()
         await flowDynamic(`Nombre: ${myState.name} \nDirección: ${myState.dir}`)
-    }).addAnswer(['¿Confirmas tus datos? Escribe el número de la opción que deseas escoger #️⃣', '1. Si ✅','2. No ❌'],{capture:true},async(ctx, {state, flowDynamic, gotoFlow}) => {
+    }).addAnswer(['¿Confirmas tus datos? Escribe  si o no dependiendo la opción que deseas escoger #️⃣', 'Si ✅','No ❌'],{capture:true},async(ctx, {state, flowDynamic, gotoFlow}) => {
 
         console.log('...')
 
 
-        if(ctx.body === '2' || ctx.body ==='No' || ctx.body ==='no'){
+        if(ctx.body ==='No' || ctx.body ==='no'){
             await state.clear(['name', 'dir']);
             return gotoFlow(flujoDatosPedido)
-        }else if (ctx.body ==='Si'|| ctx.body ==='si' || ctx.body ==='1'){
-            await flowDynamic(`Ya te registramos..`)
+        }else if (ctx.body ==='Si'|| ctx.body ==='si'){
             return gotoFlow(flujoConfirmacion)
         }
 
     })
 
 
-const flujoConfirmacion = addKeyword(['si', 'Si', '1']).addAnswer('Para estar seguros de tu pedido, haz una última revisión a tus productos y datos de domicilio:', null, async (_, { flowDynamic, state })=>{
+const flujoConfirmacion = addKeyword(['si', 'Si', 'SI']).addAnswer('Resumen de tu pedido confirmado:', null, async (_, { flowDynamic, state })=>{
     const myState = state.getMyState()
     await flowDynamic(`🍓Productos de tu orden:\n ${myState.pedido} \n \n📖Datos Domicilio: \nNombre: ${myState.name} \nDirección: ${myState.dir}`)
-}).addAnswer(['¿Tu pedido esta correcto? Escribe el número de la opción que deseas escoger #️⃣', '1. Esta mal o incompletos mis productos ✅','2. Esta mal mis datos de domicilio ❌', '3. Está todo perfecto'],{capture:true},async(ctx, {state, flowDynamic, gotoFlow}) => {
-
-    console.log('...')
-
-
-    if(ctx.body === '2' || ctx.body ==='No' || ctx.body ==='no'){
-        await flowDynamic(`Volveremos a tomarte los datos de domicilio`)
-        await state.clear(['name', 'dir']);
-        return gotoFlow(flujoDatosPedido)
-    }else if (ctx.body ==='Si'|| ctx.body ==='si' || ctx.body ==='1'){
-        await flowDynamic(`Volveremos a tomarte el pedido`)
-        return gotoFlow(flujoPedido)
+}).addAnswer('¿Necesitas algo más?:  \n1. Si, deseo volver al menú  \n2. No, muchas gracias', {capture:true}, async (ctx, { flowDynamic, state })=>{
+    if(ctx.body === '1' || ctx.body === 'Si' || ctx.body === 'si'){
+        //return gotoFlow()
     }
-    else if ( ctx.body === "3"){
-        await  flowDynamic('Pedido Confirmado')
+    else if (ctx.body === '2' || ctx.body === 'No' || ctx.body === 'no'){
+        await flowDynamic ('¡Muchisimas gracias! Ten lindo día')
     }
-
 })
 
 
